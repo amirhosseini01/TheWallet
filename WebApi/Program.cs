851c9abda6bda5106703d227ff62342c073508c8
amircsharp7 <amir.1derful@gmail.com>
2022-05-06 19:22:55 +0430

    Add Context in program.cs

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,15 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using WebApi.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+builder.Services.AddDbContext<WebApiContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
