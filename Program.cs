ccabfc91bdd1c888301b305426c63c4b252274a3
amircsharp7 <amir.1derful@gmail.com>
2022-05-06 14:44:25 +0430

    Remove Sqllite

--- Program.cs
+++ Program.cs
@@ -1,18 +1,11 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.EntityFrameworkCore;
 using TheWallet;
-using TheWallet.Data;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-builder.Services.AddDbContextFactory<TheWalletDBContext>(options =>
-      options
-        .UseSqlite("Data Source=TheWallet.db")
-        .EnableSensitiveDataLogging());
 
 await builder.Build().RunAsync();
