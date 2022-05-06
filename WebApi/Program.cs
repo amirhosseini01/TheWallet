6ad7d2905ef7c0c25205589de9a4d97956c7e440
amircsharp7 <amir.1derful@gmail.com>
2022-05-06 19:25:55 +0430

    Add New Db Exeption Cofig

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -3,8 +3,13 @@ using WebApi.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+#region DB
 builder.Services.AddDbContext<WebApiContext>(options =>
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+#endregion
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
