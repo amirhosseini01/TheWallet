78790cf17fcb0712bbcdfdba3e3fa61bc7a937ab
amircsharp7 <amir.1derful@gmail.com>
2022-05-07 21:47:17 +0430

    Chnage Finance Model And Add And Enable Migration

--- WebApi/Models/Finance.cs
+++ WebApi/Models/Finance.cs
@@ -4,9 +4,11 @@ namespace WebApi.Models;
 public class Finance : BaseModel
 {
     [Required]
+    [StringLength(250)]
     public string Type { get; set; }
     public int Amount { get; set; }
     public DateTime PayDate { get; set; }
+    [StringLength(500)]
     public string Description { get; set; }
     public DateTime CreateDate { get; set; }
     public DateTime UpdateDate { get; set; }
